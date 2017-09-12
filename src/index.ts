5c5304814d11c0f9519151913fb1d9fdb3851088
Michał [Desktop] <michal.ly@wp.pl>
2017-09-12 20:30:27 +0200

    Add support for sync validation (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { validateOrReject, validate, ValidatorOptions } from "class-validator";
+import { validate, validateSync, validateOrReject, ValidatorOptions } from "class-validator";
 import { plainToClass, ClassTransformOptions } from "class-transformer";
 
 export interface ClassType<T> {
@@ -13,7 +13,7 @@ export interface TransformValdiationOptions {
 /**
  * Asynchronously converts JSON string to class (constructor) object.
  * Reject the promise if the object parsed from string doesn't pass validation.
- * 
+ *
  * @param {ClassType<T>} classType The Class to parse and convert JSON to
  * @param {string} jsonString The string containing JSON
  * @param {TransformValdiationOptions} [options] Optional options object for class-validator and class-transformer
@@ -23,7 +23,7 @@ export function transformAndValidate<T extends object>(classType: ClassType<T>, 
 /**
  * Asynchronously converts array of plain objects to array of class (constructor) objects.
  * Reject the promise if any of the objects in array doesn't pass validation.
- * 
+ *
  * @param {ClassType<T>} classType The Class to convert object to
  * @param {object} array The array of objects to instantiate and validate
  * @param {TransformValdiationOptions} [options] Optional options object for class-validator and class-transformer
@@ -33,7 +33,7 @@ export function transformAndValidate<T extends object>(classType: ClassType<T>, 
 /**
  * Asynchronously converts plain object to class (constructor) object.
  * Reject the promise if the object doesn't pass validation.
- * 
+ *
  * @param {ClassType<T>} classType The Class to convert object to
  * @param {object} object The object to instantiate and validate
  * @param {TransformValdiationOptions} [options] Optional options object for class-validator and class-transformer
@@ -62,4 +62,61 @@ export function transformAndValidate<T extends object>(classType: ClassType<T>, 
                 .catch(reject);
         }
     });
+}
+
+/**
+ * Synchronously converts JSON string to class (constructor) object.
+ * Throws error if the object parsed from string doesn't pass validation.
+ *
+ * @param {ClassType<T>} classType The Class to parse and convert JSON to
+ * @param {string} jsonString The string containing JSON
+ * @param {TransformValdiationOptions} [options] Optional options object for class-validator and class-transformer
+ * @returns {T} Object of given class T
+ */
+export function transformAndValidateSync<T extends object>(classType: ClassType<T>, jsonString: string, options?: TransformValdiationOptions): T;
+/**
+ * Synchronously converts array of plain objects to array of class (constructor) objects.
+ * Throws error if any of the objects in array doesn't pass validation.
+ *
+ * @param {ClassType<T>} classType The Class to convert object to
+ * @param {object} array The array of objects to instantiate and validate
+ * @param {TransformValdiationOptions} [options] Optional options object for class-validator and class-transformer
+ * @returns {T[]} Array of objects of given class T
+ */
+export function transformAndValidateSync<T extends object>(classType: ClassType<T>, array: object[], options?: TransformValdiationOptions): T[];
+/**
+ * Synchronously converts plain object to class (constructor) object.
+ * Throws error if the object doesn't pass validation.
+ *
+ * @param {ClassType<T>} classType The Class to convert object to
+ * @param {object} object The object to instantiate and validate
+ * @param {TransformValdiationOptions} [options] Optional options object for class-validator and class-transformer
+ * @returns {T} Object of given class T
+ */
+export function transformAndValidateSync<T extends object>(classType: ClassType<T>, object: object, options?: TransformValdiationOptions): T;
+
+export function transformAndValidateSync<T extends object>(classType: ClassType<T>, somethingToTransform: string|object|object[], options?: TransformValdiationOptions): T {
+    let object: object;
+    if (typeof somethingToTransform === "string") {
+        object = JSON.parse(somethingToTransform);
+    } else if (somethingToTransform != null && typeof somethingToTransform === "object") {
+        object = somethingToTransform;
+    } else {
+        throw new Error("Incorrect object param type! Only string, plain object and array of plain objects are valid.");
+    }
+
+    const classObject = plainToClass(classType, object, options ? options.transformer : void 0);
+    if (Array.isArray(classObject)) {
+        const errorsArray = classObject.map(objectElement => validateSync(objectElement, options ? options.validator : void 0));
+        if (errorsArray.some(errors => errors.length !== 0)) {
+            throw errorsArray;
+        }
+        return classObject;
+    } else {
+        const errors = validateSync(classObject, options ? options.validator : void 0);
+        if (errors.length) {
+            throw errors;
+        }
+        return classObject;
+    }
 }
--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,9 +1,9 @@
-import { ValidationError, IsEmail } from 'class-validator';
-import { transformAndValidate } from "../index";
-
+import { ValidationError, IsEmail } from "class-validator";
 import { expect, use } from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 use(chaiAsPromised);
+
+import { transformAndValidate, transformAndValidateSync } from "../index";
 
 class User {
     @IsEmail()
@@ -89,7 +89,7 @@ describe("transformAndValidate()", () => {
             sampleUuser,
             sampleUuser,
         ];
-        
+
         const error: ValidationError[][] = await expect(transformAndValidate(User, users)).to.be.rejected;
 
         expect(error).to.have.lengthOf(users.length);
@@ -140,5 +140,161 @@ describe("transformAndValidate()", () => {
 
         expect(error).to.exist;
         expect(error.message).to.equals(rejectMessage);
+    });
+});
+
+describe("transformAndValidateSync()", () => {
+    let user: User;
+    const rejectMessage = "Incorrect object param type! Only string, plain object and array of plain objects are valid.";
+
+    beforeEach(() => {
+        user = {
+            email: "test@test.com",
+        } as User;
+    });
+
+    it("should successfully transform and validate User plain object", async () => {
+        const transformedUser: User = transformAndValidateSync(User, user);
+
+        expect(transformedUser).to.exist;
+        expect(transformedUser.email).to.equals("test@test.com");
+        expect(transformedUser.greet()).to.equals("Greeting");
+    });
+
+    it("should successfully transform and validate User JSON", async () => {
+        const userJson: string = JSON.stringify(user);
+
+        const transformedUser: User = transformAndValidateSync(User, userJson);
+
+        expect(transformedUser).to.exist;
+        expect(transformedUser.email).to.equals("test@test.com");
+        expect(transformedUser.greet()).to.equals("Greeting");
+    });
+
+    it("should successfully transform and validate array of User objects", async () => {
+        const users = [
+            user,
+            user,
+            user,
+        ];
+
+        const transformedUsers: User[] = transformAndValidateSync(User, users);
+
+        expect(transformedUsers).to.exist;
+        expect(transformedUsers).to.have.lengthOf(3);
+        expect(transformedUsers[0].email).to.equals("test@test.com");
+        expect(transformedUsers[1].greet()).to.equals("Greeting");
+    });
+
+    it("should throw ValidationError array when object property is not passing validation", async () => {
+        const sampleUuser = {
+            email: "test@test",
+        } as User;
+
+        try {
+            transformAndValidateSync(User, sampleUuser);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.have.lengthOf(1);
+            expect(error[0]).to.be.instanceOf(ValidationError);
+        }
+    });
+
+    it("should throw ValidationError array when json's property is not passing validation", async () => {
+        const sampleUser = {
+            email: "test@test",
+        } as User;
+        const userJson: string = JSON.stringify(sampleUser);
+
+        try {
+            transformAndValidateSync(User, userJson);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.have.lengthOf(1);
+            expect(error[0]).to.be.instanceOf(ValidationError);
+        }
+    });
+
+    it("should throw array of ValidationError arrays when properties of objects from array are not passing validation", async () => {
+        const sampleUser = {
+            email: "test@test",
+        } as User;
+        const users = [
+            sampleUser,
+            sampleUser,
+            sampleUser,
+        ];
+
+        try {
+            transformAndValidateSync(User, users);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.have.lengthOf(users.length);
+            expect(error[0]).to.have.lengthOf(1);
+            expect(error[0][0]).to.be.instanceOf(ValidationError);
+        }
+    });
+
+    it("should throw SyntaxError while parsing invalid JSON string", async () => {
+        const userJson = JSON.stringify(user) + "error";
+
+        try {
+            transformAndValidateSync(User, userJson);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.be.instanceOf(SyntaxError);
+        }
+    });
+
+    it("should throw Error when object parameter is a number", async () => {
+        try {
+            transformAndValidateSync(User, 2 as any);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.exist;
+            expect(error.message).to.equals(rejectMessage);
+        }
+    });
+
+    it("should throw Error when object parameter is a function", async () => {
+        const func = () => ({ email: "test@test.com"});
+
+        try {
+            transformAndValidateSync(User, func);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.exist;
+            expect(error.message).to.equals(rejectMessage);
+        }
+    });
+
+    it("should throw Error when object parameter is a boolean value", async () => {
+        try {
+            transformAndValidateSync(User, true as any);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.exist;
+            expect(error.message).to.equals(rejectMessage);
+        }
+    });
+
+    it("should throw Error when object parameter is a null", async () => {
+        try {
+            transformAndValidateSync(User, null as any);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.exist;
+            expect(error.message).to.equals(rejectMessage);
+        }
+    });
+
+    it("should throw Error when object parameter is an undefined", async () => {
+        try {
+            transformAndValidateSync(User, void 0 as any);
+            throw new Error("error should be throwed");
+        } catch (error) {
+            expect(error).to.exist;
+            expect(error.message).to.equals(rejectMessage);
+        }
     });
 });
